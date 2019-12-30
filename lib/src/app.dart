ca82bd26cec7141e97561b6dc4018391d8b06f9c
anotherdar <rosariodaren9@gmail.com>
2019-12-29 23:46:33 -0400

    working on counter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+//widget
+import 'package:counterapp/src/pages/home_pages.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Daren!'),
-      ),
+      home: Center(child: HomePage()),
     );
   }
 }
